2352f3cf62168d486b60682f63a35b9e649bc805
sree-r-one <sreeraj_ec@hotmail.com>
2023-04-16 12:01:31 +0800

    removed the outline from the search box

--- frontend/src/pages/table-util/GlobalFilter.jsx
+++ frontend/src/pages/table-util/GlobalFilter.jsx
@@ -20,7 +20,7 @@ const GlobalFilter = ({
   return (
     <div className="flex w-96 items-center p-2">
       <input
-        className="w-full rounded-md border-gray-300 p-2 py-2 text-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        className="w-full rounded-md border-gray-300 p-2 py-2 text-gray-500 shadow-sm outline-none focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         onChange={(e) => {
           setValue(e.target.value);
           onChange(e.target.value);
